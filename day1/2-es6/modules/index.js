8dd19949197b069ae3745c7f33e6acf8a3401484
Mehmet Seven <mehmetseven0@gmail.com>
2021-10-18 15:37:30 +0300

    es6 modules

---
+++ day1/2-es6/modules/index.js
@@ -0,0 +1,7 @@
+import Matematik, { topla as t, cikar, carp, bol } from './myModule.js';
+
+console.log(Matematik(2, 5));
+console.log(t(2, 2));
+console.log(cikar(2, 2));
+console.log(carp(2, 2));
+console.log(bol(2, 2));
---
+++ day1/2-es6/modules/myModule.js
@@ -0,0 +1,14 @@
+export const topla = (x, y) => x + y;
+export const cikar = (x, y) => x - y;
+export const carp = (x, y) => x * y;
+export const bol = (x, y) => x / y;
+
+export default topla;
+
+if (condition) {
+}
+
+if (condition) {
+}
+
+const arr = [ 1, 2, 3, 4 ]
